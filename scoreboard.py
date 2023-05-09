a3c710c75fff3e892ef764f4aa63ef5c6b812a2b
dusza7 <bartoszdusza7@gmail.com>
2023-05-09 10:51:44 +0200

    done

---
+++ scoreboard.py
@@ -0,0 +1,40 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ('Courier', 15, 'normal')
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
+        self.penup()
+        self.goto(0, 250)
+        self.pendown()
+        self.update_scoreboard()
+        self.hideturtle()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"SCORE: {self.score} High Score: {self.high_score}", False, align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", False, align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
+
+
+
